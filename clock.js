92d1065b6d51c7e873279c0406b036cabac138b6
jjnna <116959538+jjnasfoia@users.noreply.github.com>
2023-12-03 18:11:08 +0700

    Add files via upload

--- clock.js
+++ clock.js
@@ -1,52 +1,69 @@
-class Clock {
-    constructor(min, sec) {
+ export default class Clock {
+    constructor(min, sec, listComponent, id) {
         this.min = min;
         this.sec= sec;
+        this.listComponent = listComponent;
+        this.id  = id;
 
-        document.querySelector("body").innerHTML = this.codeHTML();    }
-    render () {
-        // +1s
-        let  mins = parseInt(this.min);
-        let secs  = parseInt(this.secs);
-        //
-        if (secs  < 59) {
-            secs++; 
-        } else {
-            mins++;
-            secs = 0; 
-        }
-
-        //add 0 -> num < 10
-        if (secs < 10 ) secs  = '0' +secs;
-        if (mins < 10 ) secs  = '0' +mins;
-        //return 
-        this.min  = mins;
-        this.sec  = secs;    
+       this.codeHTML ();
+    
     }
-    codeHTML  = () => {
-        let code  =``;
-        code = `<ol>
-                 <li  id  = "clock1"></li>
-                 <li  id  = "clock2"></li>
-                 <li  id  = "clock3"></li>
-                 <li  id  = "clock4"></li>
-                 <li  id  = "clock5"></li>
-                 </ol>`
-        return  code;         
-    };
-    start () { 
-        this.render();
-        console.log(this.min + ":" + this.sec);
+     printTime  () { 
+        let minute  ="";
+        let second  ="";
 
-        this.timer = setInterval (()=> {
-            this.render ();
-            console.log (this.min + ":" + this.sec); 
+        if(this.min<10) minute  = "0" + this.min;
+        else  minute  = this.min;
+        
+        if(this.sec<10) minute  = "0" + this.sec;
+        else  second  = this.sec;
+
+        let li_span  = document.querySelectorr('#' + this.id+ " span");
+        li_span.innerText  = minute + ":" + second;
+        this.listComponent.appendChild(li_span);
+     }
+    codeHTML  = () => {
+        
+        let li = document.createElement("li");
+        li.id = this.id;
+        
+        let span = document.createElement("span");
+        span.innerText  = minute + ":" + second;
+
+        let buttonStart  = document.createElement("button");
+        buttonStart.innerHTML = "Start"
+        buttonStart.addEventListener("click", this.start.bind(this));
+
+        let buttonStop = document.createElement("button");
+        buttonStop.innerHTML = "Stop"
+        buttonStop.addEventListener("click", this.stop.bind(this));
+
+        //add  in  list 
+        li.appendChild(span);
+        li.appendChild(buttonStart);
+        li.appendChild(buttonStop);
+
+        //add item in  ol (list)
+        this.listComponent.appendChild(li);
+    
+    };
+
+    updateTimeBySec() {
+        this.sec++
+        if(this.sec == 60) {
+            this.sec  = 0;
+            this.min++;
+        }
+    }
+
+    start () { 
+        this.timer.setInterval(()=> {
+            this.updateTimeBySec();
+            this.codeHTML();
         }, 1000);
     }
+
     stop() {
       clearInterval(this.timer)
   }
 }
-
-let clock1 = new Clock ("00","00");
-document.getElementById("clock1").interHTML = clock1.min+ ":" + clock1.sec;
